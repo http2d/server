86e5688c881a823cc3252bac809eec1c6685b4a0
Alvaro Lopez Ortega <alvaro@gnu.org>
2015-10-16 01:03:30 +0200

    Adds more testing functions

--- server_test.go
+++ server_test.go
@@ -6,12 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestOne(t *testing.T) {
+func TestInit(t *testing.T) {
 	var srv Server
 	var err error
 
 	err = srv.Init()
 	assert.Nil(t, err, "Could not init")
+}
 
+func TestQuit(t *testing.T) {
+	var srv Server
+	srv.Quit()
+}
+
+func TestInitQuit(t *testing.T) {
+	var srv Server
+	srv.Init()
 	srv.Quit()
 }
